cfa695fd7508d3276168307e0a966a59ef5fa0c3
Farseen <itsfarseen@gmail.com>
2025-09-11 12:12:24 +0530

    moar tests

--- tests/scripts/interactive_greeter.py
+++ tests/scripts/interactive_greeter.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
+import sys
 name = input("What is your name? ")
 print(f"Hello, {name}!")
 age = input("What is your age? ")
 print(f"You are {age} years old.")
+fail = input("Should I fail? (y/n) ")
+if fail == "y":
+    print("Failing as requested.", file=sys.stderr)
+    sys.exit(1)
+print("Exiting normally.")
+sys.exit(0)
